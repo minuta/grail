12472ac2123215eb7cd06a981cfeab6fe017be0d
AF <gammamustang@gmail.com>
2018-12-05 16:22:32 +0100

    update colors in hl.sh

--- multi-threading/hl.sh
+++ multi-threading/hl.sh
@@ -12,16 +12,16 @@ TAG_3="clone"
 TAG_4=$2      # TID 1
 TAG_5=$3      # TID 2
 
-COLOR_TAG_1="green"
-COLOR_TAG_2="yellow"
+COLOR_TAG_1="bright_green" 
+COLOR_TAG_2="bright_yellow"
 COLOR_TAG_3="red"
 COLOR_TAG_4="blue"
-COLOR_TAG_5="cyan"
+COLOR_TAG_5="bright_cyan"
 
 # remove as first step all lines till first match = "THREAD"
 cat "$1" | sed -n '/THREAD/,$p'\
-        | ack --flush --passthru --color --color-match="$COLOR_TAG_1" "$TAG_1"\
-        | ack --flush --passthru --color --color-match="$COLOR_TAG_2" "$TAG_2"\
+        | ack -i --flush --passthru --color --color-match="$COLOR_TAG_1" "$TAG_1"\
+        | ack -i --flush --passthru --color --color-match="$COLOR_TAG_2" "$TAG_2"\
         | ack -i --flush --passthru --color --color-match="$COLOR_TAG_3" "$TAG_3"\
-        | ack --flush --passthru --color --color-match="$COLOR_TAG_4" "$TAG_4"\
-        | ack --flush --passthru --color --color-match="$COLOR_TAG_5" "$TAG_5"
+        | ack -i --flush --passthru --color --color-match="$COLOR_TAG_4" "$TAG_4"\
+        | ack -i --flush --passthru --color --color-match="$COLOR_TAG_5" "$TAG_5"
