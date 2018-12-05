d33195a995ff76c4f0ac258b5f102f16742b3e61
AF <gammamustang@gmail.com>
2018-12-05 18:01:15 +0100

    update hl.sh with info header

--- multi-threading/hl.sh
+++ multi-threading/hl.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# this script works with NS-3 logs and has two tasks: 
+#  1) remove all log lines until the first match
+#  2) highlights tags/keywords with given colors in the remaining log lines
+#
 # USAGE:
 # 
-#  whithout highlightning TIDs:   ./hl.sh  <filename> 
-#  with highlightning TIDs :      ./hl.sh  <filename> <TID-1> <TID-2>
+#  without highlighting TIDs:   ./hl.sh  <filename> 
+#  with highlighting TIDs :     ./hl.sh  <filename> <TID-1> <TID-2>
 # 
 
 TAG_1="THREAD #1"
@@ -18,7 +22,6 @@ COLOR_TAG_3="red"
 COLOR_TAG_4="blue"
 COLOR_TAG_5="bright_cyan"
 
-# remove as first step all lines till first match = "THREAD"
 cat "$1" | sed -n '/THREAD/,$p'\
         | ack -i --flush --passthru --color --color-match="$COLOR_TAG_1" "$TAG_1"\
         | ack -i --flush --passthru --color --color-match="$COLOR_TAG_2" "$TAG_2"\
