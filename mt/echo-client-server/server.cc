106b334fc857970ab543d3f4c05945c727e51418
AF <>
2019-01-04 17:50:42 +0100

    add description and remove magic numbers in server.cc

--- mt/echo-client-server/server.cc
+++ mt/echo-client-server/server.cc
@@ -1,3 +1,7 @@
+//
+// this server creates a thread for each incoming connection
+// after the NUMBER_OF_THREADS connections server terminates
+//
 #include <stdio.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -6,6 +10,9 @@
 #include <pthread.h>
 
 char client_message[2000];
+const int NUMBER_OF_THREADS = 3;  // number of possible connections
+const int PORT_NUMBER = 7799;
+const char* IP = "127.0.0.1";
 
 //pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -50,10 +57,10 @@ int main(){
     serverAddr.sin_family = AF_INET;
 
     //Set port number, using htons function to use proper byte order 
-    serverAddr.sin_port = htons(7799);
+    serverAddr.sin_port = htons(PORT_NUMBER);
 
     //Set IP address to localhost 
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = inet_addr(IP);
 
     //Set all bits of the padding field to 0 
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
@@ -62,27 +69,26 @@ int main(){
     bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
     if ( listen(serverSocket, 5) == 0 )
-        printf("Listening\n");
+        printf("Listening... \n");
     else
-        printf("Error\n");
+        printf("Error while trying to listen to a socket! \n");
 
-    pthread_t tid[9];
-    //int i = 0;
+    pthread_t tid[NUMBER_OF_THREADS];
 
-    for (int i=0; i<3; i++){
+    for (int i=0; i<NUMBER_OF_THREADS; i++){
          //Accept call creates a new socket for the incoming connection
         addr_size = sizeof serverStorage;
         newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
 
         //for each client request creates a thread and assign the client request to it to process
         //so the main thread can entertain next request
-        if( pthread_create(&tid[i], NULL, socketThread, &newSocket) != 0 )
+        if ( pthread_create(&tid[i], NULL, socketThread, &newSocket) != 0 )
             printf("Failed to create thread\n");
     }
 
 
-    for (int i=0; i<3; i++){
-        pthread_join(tid[i], NULL);
+    for (int i=0; i<NUMBER_OF_THREADS; i++){
+        pthread_join ( tid[i], NULL);
     }
 
     return 0;
