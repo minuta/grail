c53096a15294d8b3052229f8006917218ce1e330
AF <>
2019-01-04 20:32:12 +0100

    add simple-client.cc

---
+++ mt/echo-client-server/simple-client.cc
@@ -0,0 +1,54 @@
+//
+// this client sends hello to server and tries to receive a response from server
+//
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h> 
+
+
+const int PORT = 7799;
+const char* IP = "127.0.0.1";
+
+
+int main(){
+
+    char message[1000];
+    char buffer[1024];
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+
+    socklen_t addr_size;
+
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(IP);
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    
+    addr_size = sizeof serverAddr;
+
+    printf("connecting to server...\n");
+    int connection_status = connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+    if (connection_status!=0){
+        printf("Error: connection failed, status-code: %d", connection_status);
+    }
+    
+    strcpy (message, "hello from client \n");
+
+    if( send(clientSocket , message , strlen(message) , 0) < 0) {
+            printf("Error: send failed! \n");
+    }
+
+    if(recv(clientSocket, buffer, 1024, 0) ) {
+        printf("Data received: %s\n", buffer);
+    } else {
+        printf("Error: receive failed! \n");
+    }
+
+    close(clientSocket);
+    
+    return 0;
+}
