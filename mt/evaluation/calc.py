f48c852762ab5747f262333817d2d5acd8023a2a
AF <gammamustang@gmail.com>
2019-02-21 20:56:09 +0100

    update calc.py

--- mt/evaluation/calc.py
+++ mt/evaluation/calc.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 # calculate an average from numbers given as program params
+# Usage:
+#   calc.py <num-1> <num-2> ... <num-n>
 
 import sys
 
@@ -6,6 +10,11 @@ numbers = [float(arg) for arg in sys.argv[1:]]
 
 sum_of_numbers = sum(numbers)
 num_of_items = len(sys.argv)-1
+
+if num_of_items == 0:
+    # raise NameError('give a list of numbers as cmd params!')
+    print ("give a list of numbers as cmd params")
+    sys.exit(1)
 
 avarage = sum_of_numbers/float(num_of_items)
 
