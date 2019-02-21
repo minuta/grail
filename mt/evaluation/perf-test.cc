d45221e47d8841515d118d792f6c9f5f8f41c3df
AF <gammamustang@gmail.com>
2019-02-21 20:57:19 +0100

    update perf-test

--- mt/evaluation/perf-test.cc
+++ mt/evaluation/perf-test.cc
@@ -7,17 +7,16 @@
  * this program creates some threads and prints thread IDs to STDOUT
  * in particular it triggers the system call clone
  *
- *
- * TODO: make an additional test which has the same code as this test, but does mutex-locking of the code:
- *          auto msg = "------> THREAD #" + std::to_string(long(threadId)) + ": hello " + std::to_string(i) + "\n";
- *          std::cout << msg;
- *
  */
 
 #include <iostream>
 #include <pthread.h>
+//#include <chrono>
+#include <time.h>
 
-const int NUM_OF_THREADS {1};
+
+
+const int NUM_OF_THREADS {1000};
 const int NUM_OF_LOOPS_IN_THREAD {10};
 
 void *threadFunction(void *threadId){
@@ -32,6 +31,14 @@ void *threadFunction(void *threadId){
 
 int main(int argc, char const *argv[])
 {
+
+    //clock_t start, end;
+    //double cpu_time_used;
+     
+
+    //start = clock();
+
+    //std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     std::cout << ">>> starting NS-3 application..." << std::endl;
 
@@ -57,6 +64,15 @@ int main(int argc, char const *argv[])
 
     
     std::cout << ">>> finishing NS-3 application..." << std::endl;
+
+    //std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+    //std::cout << "\n Execution time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" <<std::endl;
+    //std::cout << "Time difference (sec) = " << (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0 <<std::endl;
+
+    //end = clock();
+    //cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    //std::cout << "\n Execution time (sec) : " << cpu_time_used;
+
     pthread_exit(NULL);   // obsolete, due to the return statement in the main()
 
     return EXIT_SUCCESS;
