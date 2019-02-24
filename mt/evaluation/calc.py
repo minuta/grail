e003d570a50cecc3564597f2fa64525579f2d0d0
AF <gammamustang@gmail.com>
2019-02-24 01:56:08 +0100

    refactor calc.py

--- mt/evaluation/calc.py
+++ mt/evaluation/calc.py
@@ -11,27 +11,41 @@ import numpy as np
 from scipy import stats
 from math import sqrt
 
-numbers = [float(arg) for arg in sys.argv[1:]]
-num_of_items = len(sys.argv)-1
 
 # numbers = [110, 112, 106, 90, 96, 118, 108, 114, 107, 90, 85, 84, 113, 105, 90, 104]
 # num_of_items = 16
 
-if num_of_items == 0:
-    print ("give a list of numbers as cmd params")
-    sys.exit(1)
+def get_numbers_from_cmd():
+    numbers = [float(arg) for arg in sys.argv[1:]]
+    num_of_items = len(sys.argv)-1
 
-mean = np.mean(numbers)
-sd = np.std(numbers)
-variance = np.var(numbers)
-
-confidence = stats.norm.interval(0.95, loc=mean, scale=sd/sqrt(num_of_items))
-# see for details for a correct way to calculate confidence interval: 
-# https://stackoverflow.com/questions/28242593/correct-way-to-obtain-confidence-interval-with-scipy
+    if num_of_items == 0:
+        print ("give a list of numbers as cmd params")
+        sys.exit(1)
+    return numbers
 
 
-print ("- num of items        : {}".format(num_of_items))
-print ("- mean                : {}".format(mean))
-print ("- Standard deviation  : {}".format(sd))
-print ("- Variance            : {}".format(variance))
-print ("- Confidence interval : {}".format(confidence))
+def calc (num_list):
+    mean = np.mean(num_list)
+    sd = np.std(num_list)
+    variance = np.var(num_list)
+    num_of_items = len(num_list)
+
+    confidence = stats.norm.interval(0.95, loc=mean, scale=sd/sqrt(num_of_items))
+    # see for details for a correct way to calculate confidence interval: 
+    # https://stackoverflow.com/questions/28242593/correct-way-to-obtain-confidence-interval-with-scipy
+
+
+    print ("- num of items        : {}".format(num_of_items))
+    print ("- mean                : {}".format(mean))
+    print ("- Standard deviation  : {}".format(sd))
+    print ("- Variance            : {}".format(variance))
+    print ("- Confidence interval : {}".format(confidence))
+
+def main():
+    numbers_list = get_numbers_from_cmd()
+    calc(numbers_list)
+
+  
+if __name__== "__main__":
+  main()
