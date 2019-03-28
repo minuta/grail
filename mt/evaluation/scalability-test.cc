c908547a32798152650fdbb6284245be65ccc40e
AF <gammamustang@gmail.com>
2019-03-28 09:29:06 +0100

    add scalability test

---
+++ mt/evaluation/scalability-test.cc
@@ -0,0 +1,61 @@
+/*
+ * test: 
+ *       - testing multiple-thread mode and perform some math calculations 
+ *         without any I/O operations
+ */
+
+#include <iostream>
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+
+const int NUM_OF_THREADS {1000};
+const int NUM_OF_LOOPS_IN_THREAD {100};
+
+//#define NANO_SECOND_MULTIPLIER  1000000  // 1 millisecond = 1,000,000 Nanoseconds
+#define NANO_SECOND_MULTIPLIER  1000  // 1 microsecond = 1,000 Nanoseconds
+const long INTERVAL_MS = 1 * NANO_SECOND_MULTIPLIER;
+
+
+
+void *threadFunction(void *threadId){
+
+    timespec sleepValue = {0};
+    sleepValue.tv_nsec = INTERVAL_MS;
+
+    for (int i=1; i<=NUM_OF_LOOPS_IN_THREAD; i++){
+
+        nanosleep(&sleepValue, NULL); // sleep for 1 milisecond
+
+    }
+    pthread_exit(NULL); 
+}
+
+
+int main() {
+
+  
+    pthread_t threads[NUM_OF_THREADS];
+
+    for(size_t i = 1; i < NUM_OF_THREADS+1; i++){
+        //std::cout << ">>> creating THREAD #" << i << std::endl;
+        int threadCreateStatus = pthread_create (&threads[i], NULL, threadFunction, (void *)i);
+
+        if (threadCreateStatus) {
+            std::cout << ">>> error : failed to create a new THREAD #" << i << std::endl;
+        }        
+    }
+
+    // do a pthread_join(), otherwise the main thread can terminate before other 
+    // threads terminate and thus force them to terminate!
+    for(size_t i = 1; i <= NUM_OF_THREADS; i++){
+        if (pthread_join(threads[i], NULL)!=0)
+            //std::cout << ">>> THREAD #" << i << " joined successfully" << std::endl;
+            std::cout << ">>> THREAD #" << i << " couldn't be joined successfully" << std::endl;
+    }
+
+    //std::cout << ">>> finishing NS-3 application..." << std::endl;
+    pthread_exit(NULL);   // obsolete, due to the return statement in the main()
+
+    return EXIT_SUCCESS;
+}
